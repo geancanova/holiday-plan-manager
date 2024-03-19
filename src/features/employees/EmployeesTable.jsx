512d52cb761adf7d751561f3196c75921315b711
geancanova <geancanovawt@gmail.com>
2024-03-19 17:46:29 -0300

    add: pagination to components

--- src/features/employees/EmployeesTable.jsx
+++ src/features/employees/EmployeesTable.jsx
@@ -3,28 +3,32 @@ import { useEmployees } from "./useEmployees";
 import Spinner from "../../ui/Spinner";
 import Table from "../../ui/Table";
 import EmployeeRow from "./EmployeeRow";
+import Pagination from "../../ui/Pagination";
 
 function EmployeesTable({ teamId }) {
-  const { isLoading, employees } = useEmployees(teamId);
+  const { isLoading, employees, count } = useEmployees(teamId);
 
   if (isLoading) return <Spinner />;
 
   return (
-    <Table actions={false}>
-      <Table.Header>
-        <th>Name</th>
-        <th>Role</th>
-        <th>Department</th>
-        <th>Location</th>
-      </Table.Header>
+    <>
+      <Table actions={false}>
+        <Table.Header>
+          <th>Name</th>
+          <th>Role</th>
+          <th>Department</th>
+          <th>Location</th>
+        </Table.Header>
 
-      <Table.Body
-        data={employees}
-        render={(employee) => (
-          <EmployeeRow key={employee.id} employee={employee} />
-        )}
-      />
-    </Table>
+        <Table.Body
+          data={employees}
+          render={(employee) => (
+            <EmployeeRow key={employee.id} employee={employee} />
+          )}
+        />
+      </Table>
+      <Pagination count={count} />
+    </>
   );
 }
 
--- src/features/holidays/HolidayTable.jsx
+++ src/features/holidays/HolidayTable.jsx
@@ -3,26 +3,32 @@ import { useHolidays } from "./useHolidays";
 import Spinner from "../../ui/Spinner";
 import Table from "../../ui/Table";
 import HolidayRow from "./HolidayRow";
+import Pagination from "../../ui/Pagination";
 
 function HolidayTable({ planId }) {
-  const { isLoading: isLoadingHolidays, holidays } = useHolidays(planId);
+  const { isLoading: isLoadingHolidays, holidays, count } = useHolidays(planId);
 
   if (isLoadingHolidays) return <Spinner />;
 
   return (
-    <Table>
-      <Table.Header>
-        <th>Title</th>
-        <th>Description</th>
-        <th>Date</th>
-        <th></th>
-      </Table.Header>
+    <>
+      <Table>
+        <Table.Header>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Date</th>
+          <th></th>
+        </Table.Header>
 
-      <Table.Body
-        data={holidays}
-        render={(holiday) => <HolidayRow key={holiday.id} holiday={holiday} />}
-      />
-    </Table>
+        <Table.Body
+          data={holidays}
+          render={(holiday) => (
+            <HolidayRow key={holiday.id} holiday={holiday} />
+          )}
+        />
+      </Table>
+      <Pagination count={count} />
+    </>
   );
 }
 
